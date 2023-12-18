bd09fb86436090e352bb52b5e81ee9a2e0a02e14
pashkaa <55743617+pashkaa@users.noreply.github.com>
2023-12-18 11:11:29 +0200

    Update ordersReducer.ts

--- src/store/reducers/ordersReducer.ts
+++ src/store/reducers/ordersReducer.ts
@@ -17,7 +17,149 @@ type DeleteActionType = {
 type OrdersReducerActionType = DeleteActionType | SetOrdersActionType;
 
 const initialState: OrderType[] = [
-
+{
+        id: 0,
+        title: 'Длинное предлинное длинное название прихода',
+        date: '2023-01-29 12:09:33',
+        description: 'desc',
+        products: [
+          {
+            id: 1,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: 'https://i.pinimg.com/564x/1a/0c/80/1a0c809ae4d5174fe478ddb1b30d0157.jpg',
+            title: 'Product 1',
+            type: 'Monitors',
+            specification: 'Specification 1',
+            guarantee: {
+              start: '2017-06-29 12:09:33',
+              end: '2017-06-29 12:09:33'
+            },
+            price: [
+              {value: 100, symbol: 'USD', isDefault: 0},
+              {value: 2600, symbol: 'UAH', isDefault: 1}
+            ],
+            order: 1,
+            date: '2023-06-29 12:09:33'
+          },{
+            id: 2,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: 'https://i.pinimg.com/564x/1a/0c/80/1a0c809ae4d5174fe478ddb1b30d0157.jpg',
+            title: 'Product 1',
+            type: 'Monitors',
+            specification: 'Specification 1',
+            guarantee: {
+              start: '2017-06-29 12:09:33',
+              end: '2017-06-29 12:09:33'
+            },
+            price: [
+              {value: 200, symbol: 'USD', isDefault: 0},
+              {value: 2600, symbol: 'UAH', isDefault: 1}
+            ],
+            order: 1,
+            date: '2017-06-29 12:09:33'
+          },{
+            id: 3,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: 'https://i.pinimg.com/564x/1a/0c/80/1a0c809ae4d5174fe478ddb1b30d0157.jpg',
+            title: 'Product 1',
+            type: 'Monitors',
+            specification: 'Specification 1',
+            guarantee: {
+              start: '2017-06-29 12:09:33',
+              end: '2017-06-29 12:09:33'
+            },
+            price: [
+              {value: 300, symbol: 'USD', isDefault: 0},
+              {value: 2600, symbol: 'UAH', isDefault: 1}
+            ],
+            order: 1,
+            date: '2017-06-29 12:09:33'
+          }
+        ]
+      },
+      {
+        id: 1,
+        title: 'Order 2',
+        date: '2017-06-29 12:09:33',
+        description: 'desc',
+        products: [
+          {
+            id: 1,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: 'https://i.pinimg.com/564x/1a/0c/80/1a0c809ae4d5174fe478ddb1b30d0157.jpg',
+            title: 'Product 1',
+            type: 'Monitors',
+            specification: 'Specification 1',
+            guarantee: {
+              start: '2017-06-29 12:09:33',
+              end: '2017-06-29 12:09:33'
+            },
+            price: [
+              {value: 100, symbol: 'USD', isDefault: 0},
+              {value: 2600, symbol: 'UAH', isDefault: 1}
+            ],
+            order: 1,
+            date: '2017-06-29 12:09:33'
+          },{
+            id: 2,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: 'https://i.pinimg.com/564x/1a/0c/80/1a0c809ae4d5174fe478ddb1b30d0157.jpg',
+            title: 'Product 1',
+            type: 'Monitors',
+            specification: 'Specification 1',
+            guarantee: {
+              start: '2017-06-29 12:09:33',
+              end: '2017-06-29 12:09:33'
+            },
+            price: [
+              {value: 100, symbol: 'USD', isDefault: 0},
+              {value: 2600, symbol: 'UAH', isDefault: 1}
+            ],
+            order: 1,
+            date: '2017-06-29 12:09:33'
+          }
+        ]
+      },
+      {
+        id: 2,
+        title: 'Order 3',
+        date: '2017-06-29 12:09:33',
+        description: 'desc',
+        products: [
+          {
+            id: 4,
+            serialNumber: 1234,
+            isNew: 1,
+            photo: 'https://i.pinimg.com/564x/1a/0c/80/1a0c809ae4d5174fe478ddb1b30d0157.jpg',
+            title: 'Product 1',
+            type: 'Monitors',
+            specification: 'Specification 1',
+            guarantee: {
+              start: '2017-06-29 12:09:33',
+              end: '2017-06-29 12:09:33'
+            },
+            price: [
+              {value: 100, symbol: 'USD', isDefault: 0},
+              {value: 2600, symbol: 'UAH', isDefault: 1}
+            ],
+            order: 1,
+            date: '2017-06-29 12:09:33'
+          }
+        ]
+      },{
+        id: 3,
+        title: 'Order 4',
+        date: '2017-06-29 12:09:33',
+        description: 'desc',
+        products: [
+    
+        ]
+      }
 ]
 
 export const ordersReducer = (state: OrderType[] = initialState, action: OrdersReducerActionType): OrderType[]  => {
